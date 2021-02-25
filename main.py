94123f7e930778078e1f9bbf59c26bc4f590face
richardlee-kr <richard@DESKTOP-ATRA6K3.localdomain>
2021-02-25 20:56:16 +0900

    ignore config

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from user import *
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
-token = open(".token", "r").readline()
+token = open("token", "r").readline()
 
 
 @bot.event
